451c56e90566ac69bf3288ac6d91986a54c217dd
rooper <rooper3455@gmail.com>
2023-10-17 21:47:30 +0700

    search binary tree

---
+++ search.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
+
+    def _insert(self, root, key):
+        if root is None:
+            return Node(key)
+        if key < root.key:
+            root.left = self._insert(root.left, key)
+        else:
+            root.right = self._insert(root.right, key)
+        return root
+
+    def search(self, key):
+        return self._search(self.root, key)
+
+    def _search(self, root, key):
+        if root is None or root.key == key:
+            return root
+        if key < root.key:
+            return self._search(root.left, key)
+        return self._search(root.right, key)
+
+# Usage example:
+bst = BST()
+bst.insert(50)
+bst.insert(30)
+bst.insert(70)
+bst.insert(20)
+bst.insert(40)
+bst.insert(60)
+bst.insert(80)
+
+key_to_search = 60
+result = bst.search(key_to_search)
+
+if result:
+    print(f"Key {key_to_search} found in the tree.")
+else:
+    print(f"Key {key_to_search} not found in the tree.")
